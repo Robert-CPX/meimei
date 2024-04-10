bc8cb33401878d7f252e517441b026c8a83417e8
Robert-CPX <cpx@robbdev.xyz>
2024-04-10 17:54:38 +0800

    prevent the whole page from scaling when virtual keyboard pop up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -8,6 +8,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Meimei",
   description: "Your tutor",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  viewportFit: "cover",
+  minimumScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({
