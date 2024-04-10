b114bec4a93c7e7426d1890e9e6797acced5618e
Robert-CPX <cpx@robbdev.xyz>
2024-04-10 17:17:16 +0800

    auto growing input height while user typing

--- components/shared/chat/InputControl.tsx
+++ components/shared/chat/InputControl.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { FormEvent } from "react";
+import { FormEvent, useEffect, useRef } from "react";
 
 type InputControlProps = {
   prompt: string;
@@ -14,21 +14,29 @@ const InputControl = ({
   handleInput,
   loading
 }: InputControlProps) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     handleInput(prompt)
     setPrompt("")
   }
 
+  useEffect(() => {
+    const textarea = textareaRef.current
+    if (!textarea) return
+    textarea.style.height = "49px"
+    textarea.style.height = `${textarea.scrollHeight}px`
+  }, [prompt])
+
   return (
     <form onSubmit={onSubmit} className="flex">
       <div className="grow">
-        <input
+        <textarea
+          ref={textareaRef}
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
-          type="text"
           placeholder="Tell me about you today"
-          className="input h-[50px] w-full rounded-[25px] bg-[#333333] text-white opacity-50 placeholder:text-opacity-40"
+          className="no-scrollbar textarea h-12 w-full rounded-[25px] bg-[#333333] text-white opacity-50 placeholder:text-opacity-40"
         />
       </div>
     </form>
