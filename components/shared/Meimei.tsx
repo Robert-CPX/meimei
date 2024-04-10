ba3472a635014ac5c77b76f6e3dc676fd7f15b47
Robert-CPX <cpx@robbdev.xyz>
2024-04-10 15:21:01 +0800

    rollback changes since last commit

--- components/shared/Meimei.tsx
+++ components/shared/Meimei.tsx
@@ -6,49 +6,26 @@ import { useMeimei } from "@/context/MeimeiProvider"
 import { VideoSource } from "@/constants"
 
 const Meimei = () => {
-  const currentVideoRef = useRef<HTMLVideoElement | null>(null)
-  const nextVideoRef = useRef<HTMLVideoElement | null>(null)
-  const [currentVideoSrc, setCurrentVideoSrc] = useState<VideoSource | null>(meimei_0)
-  const [nextVideoSrc, setNextVideoSrc] = useState<VideoSource | null>(null)
+  const videoRef = useRef<HTMLVideoElement | null>(null)
+  const [videoSrc, setVideoSrc] = useState<VideoSource | null>(meimei_0)
   const { reaction } = useMeimei()
 
   useEffect(() => {
-    const nextVideo = nextVideoRef.current
-    if (!nextVideo) return
-
-    const nextSource = reaction === "dancing" ? meimei_1 : meimei_0
-
-    nextVideo.src = nextSource.src
-    nextVideo.load()
-    const handleCanPlay = () => {
-      const currentVideo = currentVideoRef.current
-      if (!currentVideo) return
-      nextVideo.removeEventListener("canplaythrough", handleCanPlay)
-
-      setCurrentVideoSrc(nextSource)
-      setNextVideoSrc(null)
-      currentVideoRef.current = nextVideoRef.current
-      nextVideoRef.current = currentVideo
+    if (reaction === "peaceful") {
+      setVideoSrc(meimei_0)
+    } else {
+      setVideoSrc(meimei_1)
     }
-    nextVideo.addEventListener("canplaythrough", handleCanPlay)
   }, [reaction])
 
   return (
     <div className="fixed left-0 top-0 -z-50 size-full overflow-hidden">
-      <video ref={currentVideoRef}
+      <video ref={videoRef}
         autoPlay muted playsInline loop
-        className={`size-full object-cover ${nextVideoSrc ? "hidden" : "block"}`}
-        key={currentVideoSrc?.label}
+        className="size-full object-cover"
+        key={videoSrc?.label}
       >
-        <source src={currentVideoSrc?.src} type="video/mp4" />
-      </video>
-      <video
-        ref={nextVideoRef}
-        autoPlay muted playsInline loop
-        className={`size-full object-cover ${currentVideoSrc ? "hidden" : "block"}`}
-        key={nextVideoSrc?.label}
-      >
-        <source src={nextVideoSrc?.src} type="video/mp4" />
+        <source src={videoSrc?.src} type="video/mp4" />
       </video>
     </div>
   )
