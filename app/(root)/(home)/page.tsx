dfb0e4ab6ec09101619912ffaec64d4ede7f3da9
Robert-CPX <cpx@robbdev.xyz>
2024-04-17 01:19:57 +0800

    Compatible mobile devices

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,6 +2,7 @@ import Meimei from "@/components/shared/Meimei"
 import ModeMenu from "@/components/shared/ModeMenu"
 import BrandMenu from "@/components/shared/brand-menu/BrandMenu"
 import ChatRoom from "@/components/shared/chat/ChatRoom"
+import ChatMobileBackground from "@/components/shared/chat/ChatMobileBackground"
 
 const Home = () => {
   return (
@@ -9,14 +10,17 @@ const Home = () => {
       <div className="meimei-main">
         <Meimei />
       </div>
-      <div className="flex h-full justify-between px-8 py-4 max-md:flex-col">
+      <div className="isolate flex h-full justify-between p-4 max-md:flex-col md:px-8">
         <div className="max-md:hidden">
           <BrandMenu />
         </div>
-        {/* <ModeMenu /> */}
-        <section className="flex rounded-[20px] md:basis-2/5 lg:basis-[30%]">
+        <div className="md:hidden">
+          <ModeMenu />
+        </div>
+        <div className="flex max-md:mb-4 max-md:max-h-[35%] md:basis-2/5 lg:basis-[30%]">
           <ChatRoom />
-        </section>
+        </div>
+        <ChatMobileBackground />
       </div>
     </>
   )
